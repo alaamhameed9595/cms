41e1529af06e91f1934d9d486d6c631bee2bd1cb
alaamhameed9595 <alaamhameed9595@gmail.com>
2025-06-02 11:27:44 +0300

    EditToGiveFirstUserAdminRole

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -35,7 +35,7 @@ class Post extends Model
 
     public function category()
     {
-        return $this->belongsTo(Category::class);
+        return $this->belongsTo(category::class);
     }
 
     public function tags()
